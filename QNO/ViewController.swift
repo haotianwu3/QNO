3d5087e6603e0773577ec39e49c758893239f274
Randolph <496851081@qq.com>
2016-01-11 13:02:24 +0800

    Initial Commit

---
+++ QNO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QNO
+//
+//  Created by Randolph on 11/1/2016.
+//  Copyright © 2016年 September. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

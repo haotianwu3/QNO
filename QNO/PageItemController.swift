5982a4d48ad0b68b9ff50a59b54b4a18d45715d8
Randolph <496851081@qq.com>
2016-01-11 14:43:31 +0800

    QNO initial project

---
+++ QNO/PageItemController.swift
@@ -0,0 +1,46 @@
+//
+//  PageItemController.swift
+//  QNO
+//
+//  Created by Randolph on 11/1/2016.
+//  Copyright © 2016年 September. All rights reserved.
+//
+
+import UIKit
+
+class PageItemController: UIViewController {
+    
+    var itemIndex: Int = 0
+    var imageName: String = "" {
+        didSet {
+            if let imageView = MainPageItemImageView {
+                imageView.image = UIImage(named: imageName)
+            }
+        }
+    }
+
+    @IBOutlet weak var MainPageItemImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        MainPageItemImageView!.image = UIImage(named: imageName)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- QNO/ViewController.swift
+++ QNO/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPageViewControllerDataSource {
+    
+    private var MainPageViewController: UIPageViewController?
+    
+    private let contentImages = ["img_1.png", "img_2.png", "img_3.png", "img_4.png"]
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        createPageViewController()
+        setupPageControl()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +28,73 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func createPageViewController(){
+        let pageController = self.storyboard!.instantiateViewControllerWithIdentifier("MainPageViewController") as! UIPageViewController
+        pageController.dataSource = self
+        
+        if contentImages.count > 0 {
+            let firstController = getItemController(0)!
+            let startingViewControllers: NSArray = [firstController]
+            pageController.setViewControllers(startingViewControllers as! [UIViewController], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        }
+        MainPageViewController = pageController
+        addChildViewController(MainPageViewController!)
+        self.view.addSubview(MainPageViewController!.view)
+        MainPageViewController!.didMoveToParentViewController(self)
 
+        
+    }
+    
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.grayColor()
+        appearance.currentPageIndicatorTintColor = UIColor.darkGrayColor()
+        appearance.backgroundColor = UIColor.whiteColor()
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex > 0 {
+            return getItemController(itemController.itemIndex-1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex+1 < contentImages.count {
+            return getItemController(itemController.itemIndex+1)
+        }
+        
+        return nil
+    }
+
+    
+    private func getItemController(itemIndex: Int) -> PageItemController? {
+        
+        if itemIndex < contentImages.count {
+            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("MainPageItemController") as! PageItemController
+            pageItemController.itemIndex = itemIndex
+            pageItemController.imageName = contentImages[itemIndex]
+            return pageItemController
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Page Indicator
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return contentImages.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
 }
 

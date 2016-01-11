5bf43e20880729b035e5db3080ad94a473dcbd37
aaHung <Landxh@gmail.com>
2016-01-11 20:52:27 +0800

    Add QNOAPI (empty)

---
+++ QNO/Classes/QNOAPI.swift
@@ -0,0 +1,14 @@
+//
+//  QNOAPI.swift
+//  QNO
+//
+//  Created by Xinhong LIU on 11/1/2016.
+//  Copyright © 2016 September. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class QNOAPI {
+    
+}
